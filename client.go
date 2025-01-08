00423eff96428e4f64492c777a3af571e036d218
ericksulino <ericksulino@gmail.com>
2025-01-08 20:08:16 -0300

    update

--- client.go
+++ client.go
@@ -32,7 +32,7 @@ import (
 
 const (
 	mspID        = "Org1MSP"
-	cryptoPath   = `../crypto-config/peerOrganizations/org1.example.com`
+	cryptoPath   = "../crypto-config/peerOrganizations/org1.example.com"
 	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts"
 	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore"
 	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
